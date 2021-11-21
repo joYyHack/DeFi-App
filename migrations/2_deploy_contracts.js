5e49290dc3c550896c2001c5827cd73bf3ff7aff
Tom <94608729+joYyHack@users.noreply.github.com>
2021-11-21 14:58:08 +0200

    Add a bit logic to manipulate with contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
-const TokenFarm = artifacts.require("TokenFarm");
+const TokenFarm = artifacts.require('TokenFarm');
+const DappToken = artifacts.require('DappToken');
+const DaiToken = artifacts.require('DaiToken');
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function (deployer, network, accounts) {
+  // deploy dapp tokens
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  //deploy mock dai tokens
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  //deploy token farm
+  await deployer.deploy(TokenFarm, daiToken.address, dappToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+                                      
+  //transfer all dapp tokens to token farm 
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
+
+  //transfer 100 dai tokens to investor
+  await daiToken.transfer(accounts[1], '1000000000000000000000');
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,5 +1,15 @@
 pragma solidity 0.8.10;
 
+import "./DaiToken.sol";
+import "./DappToken.sol";
+
 contract TokenFarm {
     string public name = "DAPP Token Farm";
+    DaiToken public daiToken;
+    DappToken public dappToken;
+
+    constructor(DaiToken _daiToken, DappToken _dappToken) {
+        daiToken = _daiToken;
+        dappToken = _dappToken;
+    }
 }
