b3e3185c8b1208fb308ed6cfa959497be58b0d16
joYyHack <agalaktionov5490@gmail.com>
2021-11-24 23:10:18 +0200

    Add stakeTokens func

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,8 +8,30 @@ contract TokenFarm {
     DaiToken public daiToken;
     DappToken public dappToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakersBalances;
+    mapping(address => bool) public hasStaken;
+    mapping(address => bool) public isStaking;
+
     constructor(DaiToken _daiToken, DappToken _dappToken) {
         daiToken = _daiToken;
         dappToken = _dappToken;
+    }
+
+    //deposit
+    function stakeTokens(uint _amount) public {
+        //transfer dai tokens to token farm
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // update stakers balance
+        stakersBalances[msg.sender] += _amount;
+
+        //add new stakers
+        if(!hasStaken[msg.sender]){
+            stakers.push(msg.sender);
+
+            hasStaken[msg.sender] = true;
+            isStaking[msg.sender] = true;
+        }
     }
 }
