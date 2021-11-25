ef1cb3912285400631877d54f11c850bf6ff0f1a
joYyHack <agalaktionov5490@gmail.com>
2021-11-25 10:02:45 +0200

    Add tests for stakeTokens func

--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -41,10 +41,10 @@ contract DappToken {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
-        require(_value <= allowance[_from][msg.sender]);
+        require(_value <= allowance[_from][_to]);
         balanceOf[_from] -= _value;
         balanceOf[_to] += _value;
-        allowance[_from][msg.sender] -= _value;
+        allowance[_from][_to] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
