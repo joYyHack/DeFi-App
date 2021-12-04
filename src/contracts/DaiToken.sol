a4eb148432bace96b06bdb68f1bc31c43e046b01
joYyHack <agalaktionov5490@gmail.com>
2021-12-04 18:59:18 +0200

    Add front end part and add logic to communicate to smart contracts from front

--- src/contracts/DaiToken.sol
+++ src/contracts/DaiToken.sol
@@ -23,7 +23,7 @@ contract DaiToken {
   }
 
   function transfer(address _to, uint256 _value) public returns (bool success) {
-    require(balanceOf[msg.sender] >= _value);
+    require(balanceOf[msg.sender] >= _value, "Balance must be more than 0");
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
     emit Transfer(msg.sender, _to, _value);
@@ -45,10 +45,10 @@ contract DaiToken {
     uint256 _value
   ) public returns (bool success) {
     require(_value <= balanceOf[_from]);
-    require(_value <= allowance[_from][_to]);
+    require(_value <= allowance[_from][msg.sender]);
     balanceOf[_from] -= _value;
     balanceOf[_to] += _value;
-    allowance[_from][_to] -= _value;
+    allowance[_from][msg.sender] -= _value;
     emit Transfer(_from, _to, _value);
     return true;
   }
--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -45,10 +45,10 @@ contract DappToken {
     uint256 _value
   ) public returns (bool success) {
     require(_value <= balanceOf[_from]);
-    require(_value <= allowance[_from][_to]);
+    require(_value <= allowance[_from][msg.sender]);
     balanceOf[_from] -= _value;
     balanceOf[_to] += _value;
-    allowance[_from][_to] -= _value;
+    allowance[_from][msg.sender] -= _value;
     emit Transfer(_from, _to, _value);
     return true;
   }
